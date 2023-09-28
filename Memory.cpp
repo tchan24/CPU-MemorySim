079f8a434363bbbb8d9f78ed58a9393c0368689c
tchan24 <tchan0524@gmail.com>
2023-09-28 23:35:59 +0300

    modified:   Memory.cpp

--- Memory.cpp
+++ Memory.cpp
@@ -31,6 +31,55 @@ private:
     }
 };
 
+class CPU {
+public:
+    CPU(int fd[2]) : PC(0), SP(999), IR(0), AC(0), X(0), Y(0), fd(fd) {}
+
+    void run() {
+        while (true) {
+            fetch();
+            execute();
+        }
+    }
+
+private:
+    int PC, SP, IR, AC, X, Y;
+    int fd[2];
+
+    void fetch() {
+        // Fetch the instruction at PC from memory
+        write(fd[1], &PC, sizeof(PC));
+        read(fd[0], &IR, sizeof(IR));
+        PC++;
+    }
+
+    void execute() {
+        switch (IR) {
+            case 1: // Load value
+                fetch();
+                AC = IR;
+                break;
+            case 2: // Load addr
+                fetch();
+                write(fd[1], &IR, sizeof(IR));
+                read(fd[0], &AC, sizeof(AC));
+                break;
+            case 7: // Store addr
+                fetch();
+                write(fd[1], &IR, sizeof(IR));
+                write(fd[1], &AC, sizeof(AC));
+                break;
+            case 50: // End instruction
+                terminate();
+                break;
+        }
+    }
+
+    void terminate() {
+        exit(0);
+    }
+};
+
 int main() {
     int fd[2];
     pipe(fd);
@@ -48,17 +97,10 @@ int main() {
         }
         close(fd[0]); // Close reading end of pipe
     } else {
-        // Parent Process
+        // Parent Process (CPU Process)
         close(fd[0]); // Close reading end of pipe
-        // Testing read operation
-        int address = 500;
-        write(fd[1], &address, sizeof(address));
-        int value;
-        read(fd[1], &value, sizeof(value));
-        std::cout << value << std::endl; // Output should be value at address 500 in program.txt
-        address = -1; // Sending termination signal
-        write(fd[1], &address, sizeof(address));
-        close(fd[1]); // Close writing end of pipe
+        CPU cpu(fd);
+        cpu.run();
     }
     return 0;
 }
