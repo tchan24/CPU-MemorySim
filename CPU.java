ba87ca68af8bd8120deaa890d6f1cc859bbdf2ea
tchan24 <tchan0524@gmail.com>
2023-10-01 14:19:28 +0300

    modified:   .gitignore 	modified:   CPU.java 	modified:   Memory.java

--- CPU.java
+++ CPU.java
@@ -23,7 +23,128 @@ public class CPU
     static PrintWriter pwMem;
 
     public static void main(String []args) 
-    {
+    {    
+        String inFile = args[0]; // Get input file name from command line
+        userTimer = Integer.parseInt(args[1]); // Get user timer from command line
 
+        try
+        {
+            Runtime rt = Runtime.getRuntime();
+            Process proc = rt.exec("java Memory"); // Run Memory.java
+            
+            // Set up input and output streams
+            osMem = proc.getOutputStream();
+            pwMem = new PrintWriter(osMem);
+
+            isMem = proc.getInputStream();
+            scMem = new Scanner(isMem);
+
+            pwMem.printf(inFile + "/n");
+            pwMem.flush();
+
+            while(true)
+            {
+                if ((instr % userTimer == 0) && (instr > 0) && (!interrupt))
+                {
+                    interrupt = true;
+                    kernel = false;
+                    int temp = SP;
+                    SP = sysStack;
+                    stackPush(temp);
+                    temp = PC;
+                    PC = 1000;
+                    stackPush(temp);
+                }
+
+                int instrRead = readMemory(PC, pwMem, scMem);
+                if (instrRead != -1)
+                {
+                    instructor(instrRead, pwMem, scMem);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            proc.waitFor();
+            int exitValue = proc.exitValue();
+            System.out.println("\nProcess exit - " + exitValue);
+        }
+        catch (Throwable throwable)
+        {
+            throwable.printStackTrace();
+        }
+    }
+
+    /*
+    // interrupt - false kernel - old stack > new @ 2000 after interrupt
+    static void callInterrupt()
+    {
+        kernel = false;
+        int temp = SP;
+        SP = sysStack;
+        stackPush(temp);
+        temp = PC;
+        PC = 1000;
+        stackPush(temp);
+    }
+    */
+
+    // read data - Mem to address
+    static int readMemory(int memAddr, PrintWriter pwMem, Scanner scMem)
+    {
+        if ((memAddr > 1000) && kernel)
+        {
+            System.out.println("Not authorized");
+            System.exit(0);
+        }
+
+        pwMem.printf("1, " + memAddr + "\n");
+        pwMem.flush();
+        if (scMem.hasNext())
+        {
+            String str = scMem.next();
+            if (str.isEmpty() == false)
+            {
+                return Integer.parseInt(str);
+            }
+        }
+        return -1;
+    }
+
+    // write data - Mem to address
+    static void writeMemory(int memAddr, int val, PrintWriter pwMem)
+    {
+        pwMem.printf("2, " + memAddr + ", " + val + "\n");
+        pwMem.flush(); 
+    }
+
+    // contains all the instruction types and actions
+    static void instructor(int val, PrintWriter pwMem, Scanner scMem)
+    {
+        IR = val;
+        int ops;
+
+        switch(IR)
+        {
+            //50 cases
+        }
+    }
+
+    // stack pop function - increment SP, get value from memory into temp and pop
+    static int stackPop()
+    {
+        int temp = readMemory(SP, pwMem, scMem);
+        writeMemory(SP, 0, pwMem);
+        SP++;
+        return temp;
+    }
+
+    // stack push function - decrement SP and write val to memory
+    static void stackPush(int value)
+    {
+        SP--;
+        writeMemory(SP, value, pwMem);
     }
 }
--- Memory.java
+++ Memory.java
@@ -1,10 +1,92 @@
-import java.io.*;
 import java.util.*;
-import java.lang.*;
+import java.io.*;
 
 public class Memory {
-    final static int[] mArr = new int[2000];
-    public static void main(String []args) {
 
+    final static int[] memory = new int[2000];
+    public static void main(String []args) 
+    {
+        Scanner input = new Scanner(System.in);
+        if (input.hasNextLine())
+        {
+            File file = new File(input.nextLine());
+            readFile(file);
+        }
+
+        while (true)
+        {
+            String line = null;
+            int temp;
+
+            if (input.hasNext())
+            {
+                line = input.nextLine();
+                if (line.isEmpty() == false)
+                {
+                    String[] middle = line.split(",");
+                    if (middle[0].equals("1"))
+                    {
+                        temp = Integer.parseInt(middle[1]);
+                        System.out.println(memory[temp]);
+                    }
+                    else if (middle[0].equals("2"))
+                    {
+                        int a = Integer.parseInt(middle[1]);
+                        int b = Integer.parseInt(middle[2]);
+                        memory[a] = b;
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+        input.close();
+    }
+
+    static void readFile(File file)
+    {
+        try
+        {
+            Scanner in = new Scanner(file);
+            int count = 0;
+            
+            while (in.hasNext())
+            {
+                if (in.hasNextInt())
+                {
+                    int scanNum = in.nextInt();
+                    memory[count++] = scanNum;
+                }
+                else
+                {
+                    String temp = in.next();
+                    if (temp.charAt(0) == '.')
+                    {
+                        count = Integer.parseInt(temp.substring(1));
+                    }
+                    else if (temp.equals("//")) // skip the comments in the lines of input
+                    {
+                        in.nextLine();
+                    }
+                    else
+                    {
+                        in.nextLine();
+                    }
+                }
+            }
+            in.close();
+        }
+        catch  (FileNotFoundException exception) //file not found exception thrown if the file is missing
+        {
+            throw new RuntimeException(exception);
+        }
+
+        
     }
 }
